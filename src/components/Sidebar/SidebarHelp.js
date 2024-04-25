12b7fc2c049e777d7a7bca19193fb7846131cce4
Shreyasudi45 <95220756+Shreyasudi45@users.noreply.github.com>
2024-04-25 15:55:04 +0530

    Update SidebarHelp.js

--- src/components/Sidebar/SidebarHelp.js
+++ src/components/Sidebar/SidebarHelp.js
@@ -1,5 +1,5 @@
 import { QuestionIcon } from "@chakra-ui/icons";
-import { Button, Flex, Link, Text } from "@chakra-ui/react";
+import { Button, Flex, Link, Text, useColorModeValue } from "@chakra-ui/react";
 import SidebarHelpImage from "assets/img/SidebarHelpImage.png";
 import IconBox from "components/Icons/IconBox";
 import React from "react";
@@ -7,6 +7,7 @@ import React from "react";
 export function SidebarHelp(props) {
   // Pass the computed styles into the `__css` prop
   const { children, ...rest } = props;
+  const bgColor = useColorModeValue("gray.200", "gray.800");
   return (
     <Flex
       borderRadius="15px"
@@ -18,8 +19,9 @@ export function SidebarHelp(props) {
       p="16px"
       h="170px"
       w="100%"
+      bg={bgColor}
     >
-      {/*<IconBox width="35px" h="35px" bg="white" mb="auto">
+     <IconBox width="35px" h="35px" bg="white" mb="auto">
         <QuestionIcon color="teal.300" h="18px" w="18px" />
       </IconBox>
       <Text fontSize="sm" color="white" fontWeight="bold">
@@ -27,8 +29,8 @@ export function SidebarHelp(props) {
       </Text>
       <Text fontSize="xs" color="white" mb="10px">
         Please check our docs
-  </Text>*/}
-      {/*<Link
+  </Text>*
+      <Link
         w="100%"
         href="https://demos.creative-tim.com/docs-purity-ui-dashboard/"
       >
@@ -50,7 +52,7 @@ export function SidebarHelp(props) {
         >
           DOCUMENTATION
         </Button>
-        </Link>*/}
+        </Link>
     </Flex>
   );
 }
