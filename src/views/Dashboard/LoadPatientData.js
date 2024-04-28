efb8c2ec9cf163de41c60042a51b018de33021ef
Shreyasudi45 <95220756+Shreyasudi45@users.noreply.github.com>
2024-04-28 19:28:23 +0530

    Update LoadPatientData.js

--- src/views/Dashboard/LoadPatientData.js
+++ src/views/Dashboard/LoadPatientData.js
@@ -74,7 +74,7 @@ const LoadPatientData = () => {
                 <Link to="/loadpatientdata">Load Data</Link>
               </MenuItem>
               <MenuItem>
-                <Link to="/patientscreeningroster">Upload Facesheet</Link>
+                <Link to="/patientscreeningroster">Patient Screening Roster</Link>
               </MenuItem>
               <MenuItem>
                 <Link to="/dashboard/profile">Send to Billing</Link>
